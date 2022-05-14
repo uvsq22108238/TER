5868191e8464fd1b27611a6cf9b97497cfb21f93
uvsq22108238 <91673437+uvsq22108238@users.noreply.github.com>
2022-05-14 23:02:43 +0200

    ok

--- src/Regression.py
+++ src/Regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import datasets
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
 
 
@@ -22,9 +22,9 @@ diabetes_X_test = diabetes_X[-30:]
 diabetes_y_train = diabetes_y[:-30]
 diabetes_y_test = diabetes_y[-30:]
 
-# Create MLP regression 5 NODES CORRESPONDE TO 17-2 HIDDENLAYERS
-model = MLPRegressor(hidden_layer_sizes=(5,17),
-        learning_rate_init= 0.007, max_iter=1000)
+# Create linear regression object
+model = MLPRegressor(hidden_layer_sizes=(600, 19),
+        learning_rate_init= 0.07, max_iter=1000)
 
 # Train the model using the training sets
 model.fit(diabetes_X_train, diabetes_y_train)
@@ -36,6 +36,6 @@ print(diabetes_score)
 diabetes_predicion = model.predict(diabetes_X_test)
 
 # Plot outputs
-#plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-#plt.plot(diabetes_X_test, diabetes_predicion, color="blue")
-#plt.show()
+plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
+plt.plot(diabetes_X_test, diabetes_predicion, color="blue")
+plt.show()
